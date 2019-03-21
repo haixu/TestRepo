4172dde9240cc8e576e12898753a82dbf065c317
Hai Su <hs920971@Broadcom.net>
2019-03-20 18:25:16 -0700

    progress

--- game.py
+++ game.py
@@ -7,13 +7,31 @@ class Game:
         self.__dealer = Player()
         self.__player = Player()
         self.__deck = Deck()
+        self.__playerTurn = 0
+        self.__end = False
 
     def start(self):
         self.__deck.shuffle()
         self.__dealer.hit(self.__deck.getTopCard())
+        self.__playerTurn = 1
         self.__player.hit(self.__deck.getTopCard())
+        self.__playerTurn = 0
         self.__dealer.hit(self.__deck.getTopCard())
+        self.__playerTurn = 1
         self.__player.hit(self.__deck.getTopCard())
+        self.status()
+
+    def update(self,action):
+        if(action == "H"):
+            if self.__playerTurn == 0:
+                self.__dealer.hit(self.__deck.getTopCard())
+            else:
+                self.__player.hit(self.__deck.getTopCard())
+        elif(action == "S"):
+            if self.__playerTurn == 0:
+                self.__dealer.stand(self.__deck.getTopCard())
+            else:
+                self.__player.stand(self.__deck.getTopCard())
         self.status()
 
     def status(self):
@@ -21,8 +39,18 @@ class Game:
         self.__dealer.showHand()
         print("Player:")
         self.__player.showHand()
+        if self.__dealer.getScore() > self.__player.getScore():
+            self.__end = True
+        if self.__player.getScore() > self.__dealer.getScore():
+            self.__end = True
+
+    def end(self):
+        return (self.__end)
 
 
 if __name__ == "__main__":
     game = Game()
     game.start()
+    while not game.end():
+        action = input("What Do You Want To Do (Hit/Stand/Surrender)?")
+        game.update(action)
--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player:
         self.__totalCards = 0
         self.__hand = []
         self.__totalPoint = 0
+        self.__stand = False
 
     def hit(self, card):
         if self.__totalCards <= 4:
@@ -12,22 +13,24 @@ class Player:
             self.__totalCards += 1
             self.__totalPoint += card.getValue()
         else:
-            return
+            self.__stand = True
 
     def stand(self):
-        pass
+        self.__stand = True
 
     def split(self):
         pass
 
     def surrender(self):
-        pass
+        self.__stand = True
 
     def showHand(self):
         for i in range(self.__totalCards):
             self.__hand[i].display()
         print("\nHand Value = %d" % self.__totalPoint)
 
+    def getScore(self):
+        return(self.__totalPoint)
 
 if __name__ == "__main__":
     p = Player()
